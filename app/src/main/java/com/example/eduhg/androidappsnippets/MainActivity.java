0d6f7aa997aa7c025a79199c88c7f6366fa09e35
EduhG <edwinnyangena@gmail.com>
2016-08-24 09:54:57 +0300

    Added background image to splash screen

--- app/src/main/java/com/example/eduhg/androidappsnippets/MainActivity.java
+++ app/src/main/java/com/example/eduhg/androidappsnippets/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.eduhg.androidappsnippets;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Thread bg = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(5000);
+                    Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(i);
+                    finish();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        bg.start();
     }
+
 }
