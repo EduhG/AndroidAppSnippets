5c6ea47e0fe8852f67c38d05415de9ab792890c5
EduhG <edwinnyangena@gmail.com>
2016-08-24 10:18:51 +0300

    Added background image to splash screen

--- app/src/main/java/com/example/eduhg/androidappsnippets/LoginActivity.java
+++ app/src/main/java/com/example/eduhg/androidappsnippets/LoginActivity.java
@@ -2,12 +2,24 @@ package com.example.eduhg.androidappsnippets;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = (Button) findViewById(R.id.button);
+        loginButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
